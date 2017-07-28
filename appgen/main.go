c223b4eeee455a7895c6266886ad0faab558ffcb
Robinson Ramirez <robinson@camrpc.com>
2017-07-28 18:13:29 -0400

    more text parsing

--- appgen/main.go
+++ appgen/main.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 )
 
+// LedgerDetail exported
 type LedgerDetail struct {
 	AccountNumber  byte
 	Annotation     string
@@ -23,25 +24,7 @@ type LedgerDetail struct {
 }
 
 func main() {
-	data := jsonify()
-
-	// var transaction LedgerDetail
-	// var transactions []LedgerDetail
-
-	// jsonData, err := json.Marshal(data)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	os.Exit(1)
-	// }
-
-	// jsonFile, err := os.Create("data.json")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// defer jsonFile.Close()
-	// jsonFile.Write(jsonData)
-	// fmt.Printf("%T", data)
-	// fmt.Printf("%T", jsonData)
+	sqlify()
 }
 
 func jsonify() {
@@ -62,20 +45,69 @@ func jsonify() {
 
 	var newtext []string
 	newtext = append(newtext, "[")
+
 	for scanner.Scan() {
 		lines := scanner.Text()
 
 		index := strings.Index(lines, ":")
 
 		// newline := strings.Split(lines, ":")
-		if index > 0 {
-			category := lines[:index]
+		if index >= 0 {
+			key := lines[:index]
 			values := lines[index+1:]
-			// fmt.Printf("%q:%q,", category, values)
-			newline := category + ":" + values + ","
+			fmt.Printf("%q:%q,", key, values)
+			newline := key + ":" + values + ","
 			newtext = append(newtext, newline)
 		} else {
 			newtext = append(newtext, "},\n{")
+		}
+
+		// newtext = append(newtext, newline)
+	}
+
+	fmt.Println(newtext)
+
+	// for i := 0; i < 12; i++ {
+	// 	fmt.Printf(record[i][0])
+
+	// }
+
+}
+
+func sqlify() {
+	// Open file
+	file, err := os.Open("testquery.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	// Scan .txt file into buffer and check for errors.
+	scanner := bufio.NewScanner(file)
+	if err != nil {
+		panic(err)
+	}
+
+	// scanner.Split(splitcolon)
+
+	var newtext []string
+	// newtext = append(newtext, "[")
+
+	for scanner.Scan() {
+		lines := scanner.Text()
+
+		index := strings.Index(lines, ":")
+
+		// newline := strings.Split(lines, ":")
+		if index >= 0 {
+			// key := lines[:index]
+			values := lines[index+1:]
+			// fmt.Printf("%q:%q,", key, values)
+			// newline := key + ":" + values + ","
+			newline := values + "','"
+			newtext = append(newtext, newline)
+		} else {
+			newtext = append(newtext, "),\n(")
 		}
 
 		// newtext = append(newtext, newline)
